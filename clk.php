eee8d73784d7d685cb1281d3202a16a5616ca419
Rico <jerrykaduma@gmail.com>
2018-04-13 11:39:15 +0100

    added PHP, background image and CSS files

---
+++ clk.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>
+        Clock
+    </title>
+    <link href="style.css" rel="stylesheet">
+    
+
+
+    <body >
+        <center>
+            
+        
+            <div id="clck" class="clock" >
+            <?php echo date("H:i:sa") ?>
+            </div>
+        
+
+        </center>
+    </body>
+</head>
+
+</html>
